53f8281a752f880041ea92d077c458c55b44d96e
Porshen Lai <porshenlai@gmail.com>
2025-03-11 16:51:50 +0800

    divide project into DevPiers and ChihleeSmartPiers

--- PWS/bin/play.sh
+++ PWS/bin/play.sh
@@ -3,12 +3,15 @@ ROOT=$(realpath $0) && ROOT=${ROOT%/bin/*}
 DOCS=${ROOT}/docs
 PID=${ROOT}/PWS.pid
 
+python3="__pyenv__/bin/python3"
+test -f "__pyenv__/python.exe" && python3="__pyenv__/python.exe"
+
 while test "$1"; do
 case "$1" in
 start)
 	if test -f "${ROOT}/config.json"; then
 		cd ${ROOT}
-		__pyenv__/bin/python3 ${ROOT}/daemon.py ${ROOT}/config.json &
+		${python3} ${ROOT}/daemon.py ${ROOT}/config.json &
 		for s in 1 2 3 4 5 6 7 8 9 10; do
 			if test -f "${PID}"; then
 				echo "Daemon running at " $(cat ${PID})
